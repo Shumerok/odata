531c7cfeacae27f861e2c1dec95fd5bee2403430
shumerok <shumerok@gmail.com>
2023-03-06 23:11:15 +0200

    change type of params in getMoneyCheck

--- app/Services/OdataService.php
+++ app/Services/OdataService.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\MoneyCheck;
+use DateTimeImmutable;
 use Kily\Tools1C\OData\Client;
 
 class OdataService
 {
-    public function getMoneyCheck(string $date_start = '2022-10-01', string $date_end = '2022-12-31'): array
-    {
+    public function getMoneyCheck(
+        DateTimeImmutable $date_start = new DateTimeImmutable('2022-10-01'),
+        DateTimeImmutable $date_end = new DateTimeImmutable('2022-12-31')
+    ): array {
         $client = new Client('https://ru.enote.link/08efd710-4709-478f-bac1-2d1b7e209599/odata/standard.odata/', [
             'auth' => [
                 env('ODATA_LOGIN'),
@@ -19,7 +22,7 @@ class OdataService
         ]);
 
         return $client->{'Document_ДенежныйЧек'}->filter(
-            "Date ge datetime'{$date_start}T00:00:00' and Date le datetime'{$date_end}T23:59:59'"
+            "Date ge datetime'{$date_start->format('Y-m-d')}T00:00:00' and Date le datetime'{$date_end->format('Y-m-d')}T23:59:59'"
         )->get()->values();
     }
 
